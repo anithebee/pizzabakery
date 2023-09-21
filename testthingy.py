16c815f5e183f8279e66c975f1ad5d271d03050b
anithebee <143959897+anithebee@users.noreply.github.com>
2023-09-21 20:33:52 +0530

    Add files via upload

--- testthingy.py
+++ testthingy.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+#page config
+
 st.set_page_config(page_title="Pizza Bakery", page_icon = ":pizza:")
 st.title("Papa's Pizzeria (walmart version)")
 st.header("Menu:pizza: || 1 Plain Pizza is 3 coins!")
@@ -9,9 +11,9 @@ st.write("(2) Olives cost 2 coins.")
 st.write("(3) Double Cheese costs 4 coins.")
 st.write("(4) Chicken costs 6 coins.")
 st.write("(5) Pineapple costs 2 coins.")
-
 st.header("Place Your Order")
 
+#base pizza
 number_of_pizzas  = st.number_input("How many pizzas do you want?")
 if number_of_pizzas<0:
     st.write("You want...less than 1 pizza-? A negative pizza? STOP TORMENTING OUR WAITERS!")
@@ -26,9 +28,9 @@ if number_of_pizzas>30:
 
 cost_per_pizza = 3
 
+#toppings
 toppings = ["Pepperoni", "Olives", "Double Cheese", "Chicken", "Pineapple"]
 toppingsprice = [3, 2, 4, 6, 2]
-
 
 toppingschoice = st.number_input("Which topping would you like? Select from 1-5. If you do not want a topping, click 0.")
 
@@ -50,10 +52,10 @@ if toppingschoice == 5 :
     st.write("hippie waiter: PINEAPPLE IS SUCH A GOOD TOPPING, I DONT CARE WHAT ANYONE SAYS, BYGODISWEARTHATTHESWEETMAKESTHESAVOUR- *nervous breakdown*")
 if toppingschoice == 0:
     extracost = 0
-
 if toppingschoice != 0 and toppingschoice != 1 and toppingschoice != 2 and toppingschoice != 3 and toppingschoice != 4 and toppingschoice != 5:
      st.write("Our waiter is very confused. Can you read?")
-    
+
+#bill
 
 if toppingschoice ==  0 and number_of_pizzas == 1:
     st.write(number_of_pizzas, " Margherita Pizza coming right up!...")
@@ -82,29 +84,32 @@ if toppingschoice ==  5 and number_of_pizzas>1:
     st.write(number_of_pizzas, " Pineapple Pizzas coming right up!...")
 
 st.subheader("Your Bill :money_with_wings:")
-st.write("Base Quantity: ", number_of_pizzas)
+st.write("Base Pizza Price: 3")
 if toppingschoice == 1:
-    st.write("Topping: Pepperoni")
+    st.write(":right-arrow: Topping: Pepperoni, Price: 3")
 if toppingschoice == 2:
-    st.write("Topping: Olives")
+    st.write(":right-arrow: Topping: Olives, Price: 2")
 if toppingschoice == 3:
-    st.write("Topping: Double Cheese")
+    st.write(":right-arrow: Topping: Double Cheese, Price: 4")
 if toppingschoice == 4:
-    st.write("Topping: Chicken")
+    st.write(":right-arrow: Topping: Chicken, Price: 6")
 if toppingschoice == 5:
-    st.write("Topping: Pineapple")
+    st.write(":right-arrow: Topping: Pineapple, Price: 2")
 if toppingschoice == 0:
-    st.write("Topping: N/A")
-
+    st.write(":right-arrow: Topping: N/A, Price: N/A")
+    
+if toppingschoice!=0:
+st.write(":right-arrow: Total Base Price: ", cost_per_pizza + extracost)
+st.write(":right-arrow: Quantity: ", number_of_pizzas)
 
 if toppingschoice != 0:
     subtotal = number_of_pizzas * (cost_per_pizza + extracost)
-    st.write("The cost before tax is: ", subtotal)
+    st.write("Subtotal: ", subtotal)
     tax_rate = 0.08
     sales_tax = subtotal * tax_rate
     total = subtotal + sales_tax
-    fornow3 =  st.write("The total cost is $", total)
-    fornow4 = st.write("This includes ", subtotal, " coins for the food and  ", sales_tax, " coins in sales tax.")
+    fornow3 =  st.write("Total (including tax): ", total)
+    fornow4 = st.write("/n This includes ", subtotal, " coins for the food and  ", sales_tax, " coins in sales tax.")
 
 if toppingschoice == 0:
     fornow1 = st.write("The cost before tax is: ", number_of_pizzas * cost_per_pizza)
